021700401a2ec22d1c9b2fa465cc96fa0f6c4613
itcast <1156752496@qq.com>
2019-06-16 20:17:14 +0800

    修改app.java

---
+++ git1/src/main/java/cn/itheima/App.java
@@ -0,0 +1,17 @@
+package cn.itheima;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+        System.out.println("aaaaaaa");
+        System.out.println("aaaaaaa");
+        System.out.println("aaaaaaa");
+    }
+}
